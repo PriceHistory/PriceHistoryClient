cfc7ac034dfb566b4b14f53127decff6e1e01ac9
vgotsuliak <liddder@gmail.com>
2016-06-11 23:13:18 +0300

    basic layout of application

---
+++ src/app/js/PriceHistory.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var priceHistoryClient = angular.module('PriceHistoryClient', []);
+
+priceHistoryClient.controller('PriceHistoryCtrl', ['$scope', function ($scope) {
+    console.log('here i need to do smth');
+}]);
