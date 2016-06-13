05127ab7bfde22354886d0e7559fd3ec8a1dbe42
vgotsuliak <liddder@gmail.com>
2016-06-13 21:24:06 +0300

    mock server return only one needed product with prices by ID

--- src/app/mock/app.js
+++ src/app/mock/app.js
@@ -9,7 +9,12 @@ app.use(cors());
 
 app.get('/price-history/:productID', function (req, res) {
     console.log('Retrieving price history for product with id: ' + req.params.productID);
-    res.send(products);
+    products.forEach(function(item, i, arr) {
+        if (req.params.productID == item.ProductID) {
+            res.send(item);
+        }    
+    });
+    res.sendStatus(404);
 });
 
 app.listen(8001, function () {
